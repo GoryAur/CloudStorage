14161bf6312771f515e6281080da10570fa4b9f8
GoryAur <gory.aur@gmail.com>
2022-06-17 02:00:31 -0400

    who cares

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,12 +2,10 @@ const { Router } = require('express');
 const getFiles = require('../controllers');
 const signIp = require('../middleware/signIp');
 const streaming = require('../controllers/stream');
-const getUsers = require('../controllers/mysql');
 
 const router = Router()
 
-router.get('/user', getUsers)
 router.get('/:file', signIp, getFiles)
-router.get('/video/:file', streaming)
+// router.get('/video/:file', streaming)
 
 module.exports = router
